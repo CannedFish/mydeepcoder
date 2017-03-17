859aeaaf31c27d3bd50dc9ce6b7db8dcce8c60ae
CannedFish <lianggy0719@126.com>
2017-03-17 17:19:10 +0800

    Generate programs

--- data_gen.py
+++ data_gen.py
@@ -2,16 +2,61 @@
 
 import sys
 
-from dsl import First_Order, High_Order, Lambdas
+from dsl import FUNCs, Lambdas
+
+PROGRAM = []
 
 def save(f_path):
     """
     Save data to a file
     """
-    print f_path
+    with open(f_path, 'w+') as f:
+        f.writelines(map(lambda x: str(x)[1:-1]+'\n', PROGRAM))
+    print "Generated programs has been saved at %s" % f_path
+
+def search_next(cur, program, fo_remain, ho_remain, target_len):
+    if len(program) == target_len:
+        PROGRAM.append(program)
+        print program
+        return
+    else:
+        # NOTE: tunable
+        # How can I put functions like ZIPWITH after 2 functions which 
+        # produce an integer array?
+        valid_fo_funcs = filter(lambda x: FUNCs['FO'][x][1][0] == cur[2] \
+                if len(FUNCs['FO'][x][1]) == 1 \
+                else FUNCs['FO'][x][1][1] == cur[2], fo_remain)
+        valid_ho_funcs = filter(lambda x: len(FUNCs['HO'][x][1]) == 2 \
+                and FUNCs['HO'][x][1][1] == cur[2], ho_remain)
+        for func in valid_fo_funcs:
+            search_next(FUNCs['FO'][func], \
+                    program+[func], \
+                    filter(lambda x: x!=func, valid_fo_funcs), \
+                    valid_ho_funcs, \
+                    target_len)
+        for func in valid_ho_funcs:
+            search_next(FUNCs['HO'][func], \
+                    program+[func], \
+                    valid_fo_funcs, \
+                    filter(lambda x: x!=func, valid_ho_funcs), \
+                    target_len)
 
 def main(length, f_path):
-    print length
+    print "Generating programs whose length is %d" % length
+    fo_funcs = [func for func in FUNCs['FO']]
+    ho_funcs = [func for func in FUNCs['HO']]
+    for func in fo_funcs:
+        search_next(FUNCs['FO'][func], \
+                [func], \
+                filter(lambda x: x!=func, fo_funcs), \
+                ho_funcs, \
+                length)
+    for func in ho_funcs:
+        search_next(FUNCs['HO'][func], \
+                [func], \
+                fo_funcs, \
+                filter(lambda x: x!=func, ho_funcs), \
+                length)
     save(f_path)
 
 if __name__ == '__main__':
@@ -21,6 +66,6 @@ if __name__ == '__main__':
                 "Param $File: the path of file to save"
         sys.exit(1)
 
-    f_path = './generated_data' if len(sys.argv)<3 else sys.argv[2]
-    main(sys.argv[1], f_path)
+    f_path = './generated_data.dat' if len(sys.argv)<3 else sys.argv[2]
+    main(int(sys.argv[1]), f_path)
 
--- dsl.py
+++ dsl.py
@@ -1,23 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# 0 -> int, 1 -> [int], 2 -> bool, 3 -> lambda(int), 4 -> lambda(int, int)
-TYPE = ['int', '[int]', 'bool']
-
-# [func, input type, output type]
-# First Order
-First_Order = {
-    'HEAD': [lambda v: v[0] if len(v)>0 else None, (1,), 0],
-    'LAST': [lambda v: v[-1] if len(v)>0 else None, (1,), 0],
-    'TAKE': [lambda n, v: v[:n], (0, 1), 1],
-    'DROP': [lambda n, v: v[n:], (0, 1), 1],
-    'ACCESS': [lambda n, v: v[n] if n>=0 and len(v)>n else None, (0, 1), 1],
-    'MINIMUM': [lambda v: min(v) if len(v)>0 else None, (1,), 0],
-    'MAXIMUM': [lambda v: max(v) if len(v)>0 else None, (1,), 0],
-    'REVERSE': [lambda v: list(reversed(v)), (1,), 1],
-    'SORT': [lambda v: sorted(v), (1,), 1],
-    'SUM': [lambda v: sum(v), (1,), 1],
-}
- 
 def SCANL1(f, v):
     if len(v) > 0:
         y = [v[0]] * len(v)
@@ -27,13 +9,32 @@ def SCANL1(f, v):
     else:
         return None
 
+# 0 -> int, 1 -> [int], 2 -> bool, 3 -> lambda(int), 4 -> lambda(int, int)
+TYPE = ['int', '[int]', 'bool']
+
+# [func, input type, output type]
+FUNCs = {
+# First Order
+    'FO': {
+        'HEAD': [lambda v: v[0] if len(v)>0 else None, (1,), 0],
+        'LAST': [lambda v: v[-1] if len(v)>0 else None, (1,), 0],
+        'TAKE': [lambda n, v: v[:n], (0, 1), 1],
+        'DROP': [lambda n, v: v[n:], (0, 1), 1],
+        'ACCESS': [lambda n, v: v[n] if n>=0 and len(v)>n else None, (0, 1), 1],
+        'MINIMUM': [lambda v: min(v) if len(v)>0 else None, (1,), 0],
+        'MAXIMUM': [lambda v: max(v) if len(v)>0 else None, (1,), 0],
+        'REVERSE': [lambda v: list(reversed(v)), (1,), 1],
+        'SORT': [lambda v: sorted(v), (1,), 1],
+        'SUM': [lambda v: sum(v), (1,), 0],
+    },
 # High Order
-High_Order = {
-    'MAP': [lambda f, v: [f(x) for x in v], (3, 1), 1],
-    'FILTER': [lambda f, v: [x for x in v if f(x)], (3, 1), 1],
-    'COUNT': [lambda f, v: len([x for x in v if f(x)]), (3, 1), 1],
-    'ZIPWITH': [lambda f, v1, v2: [f(x, y) for x, y in zip(v1, v2)], (4, 1, 1), 1],
-    'SCANL1': [SCANL1, (4, 1), 1]
+    'HO': {
+        'MAP': [lambda f, v: [f(x) for x in v], (3, 1), 1],
+        'FILTER': [lambda f, v: [x for x in v if f(x)], (3, 1), 1],
+        'COUNT': [lambda f, v: len([x for x in v if f(x)]), (3, 1), 0],
+        'ZIPWITH': [lambda f, v1, v2: [f(x, y) for x, y in zip(v1, v2)], (4, 1, 1), 1],
+        'SCANL1': [SCANL1, (4, 1), 1]
+    }
 }
 
 # [lambda, input type, output type, lambda type]
@@ -55,7 +56,7 @@ Lambdas = {
     '(%2==1)': [lambda x: x%2==1, (0,), 0, 3],
     '(+)': [lambda x, y: x+y, (0, 0), 0, 4],
     '(-)': [lambda x, y: x-y, (0, 0), 0, 4],
-    '(*)': [lambda x, y, x*y, (0, 0), 0, 4],
+    '(*)': [lambda x, y: x*y, (0, 0), 0, 4],
     'MIN': [lambda x, y: x if x<y else y, (0, 0), 0, 4],
     'MAX': [lambda x, y: x if x>y else y, (0, 0), 0, 4],
 }
